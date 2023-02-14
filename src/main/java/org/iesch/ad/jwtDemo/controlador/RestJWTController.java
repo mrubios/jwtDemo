e170afbc090463d8ed039eb41fad050ec2cbd3c6
dam2 <mrubios>
2023-02-14 17:30:07 +0100

    Mensaje para los administradores("Todavia no hay roles metidos")

--- src/main/java/org/iesch/ad/jwtDemo/controlador/RestJWTController.java
+++ src/main/java/org/iesch/ad/jwtDemo/controlador/RestJWTController.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.iesch.ad.jwtDemo.servicio.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +37,17 @@ public class RestJWTController {
     public ResponseEntity<?> comprueba(@RequestParam String jwt){
         Jws nuestroJwt = jwtService.parseJwt(jwt);
         return ResponseEntity.ok(nuestroJwt);
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<?> getMensajeAdmin(){
+        //Esto nos mostrará la autenticación
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        log.info("Datos del usuario: {}", auth.getPrincipal());
+        log.info("Datos de los permisos: {}", auth.getAuthorities());
+        log.info("Esta autenticado: {}", auth.isAuthenticated());
+        Map<String, String> mensaje = new HashMap<>();
+        mensaje.put("Contenido", "Mensaje que solo puede ver el administrador");
+        return ResponseEntity.ok(mensaje);
     }
 }
